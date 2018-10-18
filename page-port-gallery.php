e05626f71badc8ee63f2ed36e732ed3ad67a138d
switchstancezackeaton <zack.eaton@switchstanceit.com>
2018-10-18 15:27:43 +0100

    new gallery bits and blog

--- page-port-gallery.php
+++ page-port-gallery.php
@@ -5,21 +5,16 @@
 get_header(); ?>
 
 <div class="twelve columns content">
-
-        <div class="offset-by-one ten columns content-inner">
-  
-            <div class="gallery-inner">
-                 <?php if ( have_posts() ) : 
-                    while ( have_posts() ) : 
-                            the_post(); 
-                                 the_content(); 
-                     endwhile; else : ?>
-                    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-                <?php endif; ?>
-            </div>
-           
-        </div>         
+    <div class="content-inner gallery">
+        <div class="gallery-inner">
+             <?php if ( have_posts() ) : 
+                while ( have_posts() ) : 
+                        the_post(); 
+                             the_content(); 
+                 endwhile; else : ?>
+                <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+            <?php endif; ?>
+        </div>
+    </div>         
 </div>
-
-
 <?php get_footer(); ?>
